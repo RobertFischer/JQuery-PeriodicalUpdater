19b9bb677448869e5a55c4ba6b972fe342051eb0
swarmingbee <shughes@hugheshoney.com>
2011-05-04 13:14:52 -0700

    Edited jquery.periodicalupdater.js via GitHub

--- jquery.periodicalupdater.js
+++ jquery.periodicalupdater.js
@@ -147,7 +147,7 @@
               if(remoteData == null) remoteData = rawData;
               // jQuery 1.4+ $.ajax() automatically converts "data" into a JS Object for "type:json" requests now
               // For compatibility with 1.4+ and pre1.4 jQuery only try to parse actual strings, skip when remoteData is already an Object
-              if((ajaxSettings.dataType === 'json') && (typeof(remoteData) === 'string')) {
+              if((ajaxSettings.dataType === 'json') && (typeof(remoteData) === 'string') && (success == "success")) {
                 remoteData = JSON.parse(remoteData);
               }
               if(settings.success) { settings.success(remoteData, success, xhr, handle); }
